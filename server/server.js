422bd7964ef2e98f1fb03ddd72c12c25a5caaec1
jothinayagan <v.i.jothinayagan.307@gmail.com>
2021-05-07 20:23:57 +0530

    + Server setup

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+    console.log("User connected!");
+
+    // Notify user has been left the session/chat
+    socket.on("disconnect", () => console.log("User had left chat!"));
+});
+
+app.get("/", (_req, res) => res.send(`<h1>Server is running!!</h1>`));
+
+server.listen(PORT, () => console.log(`Server up & listening to port ${PORT}`));
