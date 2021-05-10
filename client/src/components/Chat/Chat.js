3123642f8555d33b2acbdfe6e825511a596dbe85
jothinayagan <v.i.jothinayagan.307@gmail.com>
2021-05-10 14:49:35 +0530

    + Client: Input compo added

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,6 +3,8 @@ import queryString from "query-string";
 import io from "socket.io-client";
 
 import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
 
 import "./Chat.css";
 
@@ -55,13 +57,11 @@ const Chat = ({ location }) => {
         <div className="outerContainer">
             <div className="container">
                 <InfoBar room={room} />
-                {/* <input
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    onKeyPress={(e) =>
-                        e.key === "Enter" ? sendMessage(e) : null
-                    }
-                /> */}
+                <Input
+                    message={message}
+                    setMessage={setMessage}
+                    sendMessage={sendMessage}
+                />
             </div>
         </div>
     );
