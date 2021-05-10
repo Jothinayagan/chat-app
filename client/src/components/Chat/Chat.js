75277864ec2b90d3802562c305d8135cb0225f4b
jothinayagan <v.i.jothinayagan.307@gmail.com>
2021-05-10 14:07:01 +0530

    + Client: Listening and sending messages

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
+import InfoBar from "../InfoBar/InfoBar";
+
+import "./Chat.css";
+
 let socket;
 
 const Chat = ({ location }) => {
     const [name, setName] = useState("");
     const [room, setRoom] = useState("");
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = `localhost:4000`;
 
     useEffect(() => {
@@ -17,10 +23,48 @@ const Chat = ({ location }) => {
             transports: ["websocket", "polling", "flashsocket"],
         });
 
-        console.log(socket);
+        setName(name);
+        setRoom(room);
+
+        socket.emit("join", { name, room }, () => {});
+
+        return () => {
+            socket.emit("disconnect"); // emit disconnect event to backend
+            socket.off(); // turn off the user/instance
+        };
     }, [ENDPOINT, location.search]);
 
-    return <h1>Chat Component</h1>;
+    // Check for new messages!!
+    useEffect(() => {
+        socket.on("message", (message) => {
+            setMessages([...messages, message]);
+        });
+    }, [messages]);
+
+    // function for sending messages
+    const sendMessage = (event) => {
+        event.preventDefault();
+        if (message) {
+            socket.emit("sendMessage", message, () => setMessage(""));
+        }
+    };
+
+    console.log(message, messages);
+
+    return (
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room} />
+                {/* <input
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    onKeyPress={(e) =>
+                        e.key === "Enter" ? sendMessage(e) : null
+                    }
+                /> */}
+            </div>
+        </div>
+    );
 };
 
 export default Chat;
